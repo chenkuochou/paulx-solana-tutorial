97a1dba94093740cf210fa140ea480bab5d9917f
Chen Chou <39699466+chenkuochou@users.noreply.github.com>
2022-02-08 15:45:50 +1030

    .

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,13 @@
 pub mod entrypoint;
+
+// ├─ src
+// │  ├─ lib.rs -> registering modules
+// │  ├─ entrypoint.rs -> entrypoint to the program
+// │  ├─ instruction.rs -> program API, (de)serializing instruction data
+// │  ├─ processor.rs -> program logic
+// │  ├─ state.rs -> program objects, (de)serializing state
+// │  ├─ error.rs -> program specific errors
+// ├─ .gitignore
+// ├─ Cargo.lock
+// ├─ Cargo.toml
+// ├─ Xargo.toml
